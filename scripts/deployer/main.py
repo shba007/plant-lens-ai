02bb2a4f373add450143a2f401d7ea536f47785a
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-12-14 23:06:54 +0530

    feat: preprocessing changed

--- scripts/deployer/main.py
+++ scripts/deployer/main.py
@@ -1,13 +1,18 @@
+# import tensorflowjs as tfjs
 import subprocess
 
 ROOT_PATH = "D:/Programming/Projects/Public/plant-lens/ai"
 DEVELOPMENT_MODEL_PATH = f"{ROOT_PATH}/model/development"
 DEPLOYMENT_MODEL_PATH = f"{ROOT_PATH}/model/deployment"
-VERSION_TAG = "0.0.0-51"  # input("Enter Version Tag (e.g 0.0.0-0):")
+VERSION_TAG = "0.1.0-37"  # input("Enter Version Tag (e.g 0.0.0-0):")
 
 
 def main():
-    command = f"tensorflowjs_converter --input_format=tf_saved_model --saved_model_tags=serve {DEVELOPMENT_MODEL_PATH}/v{VERSION_TAG} {DEPLOYMENT_MODEL_PATH}/v{VERSION_TAG}"
+    # model = tf.keras.models.load_model(
+    # f"{DEVELOPMENT_MODEL_PATH}/v{VERSION_TAG}")
+    # model.summary()
+    # tfjs.converters.save_keras_model(model, "tfjs_model")
+    command = f"tensorflowjs_converter --input_format=keras --output_format=tfjs_layers_model {DEVELOPMENT_MODEL_PATH}/v{VERSION_TAG} {DEPLOYMENT_MODEL_PATH}/v{VERSION_TAG}"
     print(command)
 
     # Run the command
